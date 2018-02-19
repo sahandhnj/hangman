017b35bee580ccc7cdc58d8148a5705a1d2be672
Sahand <sahand.hnj@gmail.com>
2018-02-19 23:24:18 +0100

    graphql api interface for frontend

---
+++ client/src/app/api.ts
@@ -0,0 +1,23 @@
+
+export interface getAllQuery {
+    gameList: Array<{
+      __typename: 'Game';
+      playername: string | null;
+      answers: [string] | null;
+      id: string | null;
+    } | null> | null;
+  }
+  
+  export interface newGameMutationVariables {
+    playername: string;
+  }
+  
+  export interface newGameMutation {
+    createGame: {
+      __typename: 'Game';
+      playername: string | null;
+      answers: [string] | null;
+      id: string | null;
+    } | null;
+  }
+  
